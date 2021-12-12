947b1855cf9dcd612c39a65695a063006e5fb6e4
darshanpangare <darshanpangare@gmail.com>
2021-12-11 23:28:14 -0600

    Updates

--- data_analysis.py
+++ data_analysis.py
@@ -5,35 +5,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import pearsonr
 
-df = pd.read_csv("/Users/dp/Desktop/SEM 1/IS597PR/Final_Project/Datasets/Food_dataset/wfp_market_food_prices.csv", encoding = 'unicode_escape')
-# df
+def get_hyp1_data():
+    """
 
-#Rename all the columns
-food_data = df.rename(columns={"adm0_id": "country_id", "adm0_name": "country_name", "adm1_id": "locality_id", "adm1_name": "locality_name", "mkt_id": "market_id", "mkt_name": "market_name", "cm_id": "commodity_id", "cm_name": "commodity_name", "cur_id" : "currency_id", "cur_name" : "currency_name", "pt_id" : "market_type_id", "pt_name" : "market_type", "um_id": "measurement_id", "um_name" : "unit", "mp_month": "month", "mp_year": "year", "mp_price": "price", "mp_commoditysource" : "source_information"})
-# food_data
+    :return:
+    """
+    df = pd.read_csv("/Users/dp/Desktop/SEM 1/IS597PR/Final_Project/Datasets/Food_dataset/wfp_market_food_prices.csv")
+    food_data = df.rename(columns={"adm0_id": "country_id", "adm0_name": "country_name", "adm1_id": "locality_id", "adm1_name": "locality_name", "mkt_id": "market_id", "mkt_name": "market_name", "cm_id": "commodity_id", "cm_name": "commodity_name", "cur_id" : "currency_id", "cur_name" : "currency_name", "pt_id" : "market_type_id", "pt_name" : "market_type", "um_id": "measurement_id", "um_name" : "unit", "mp_month": "month", "mp_year": "year", "mp_price": "price", "mp_commoditysource" : "source_information"})
+    country_data = pd.read_excel("/Users/dp/Desktop/SEM 1/IS597PR/Final_Project/Datasets/Food_dataset/Country_status_price.xlsx")
+    comb_data = food_data.merge(country_data, left_on = 'country_name', right_on = 'country_name')
+    comb_data['usd_price'] = comb_data['price'] * comb_data['price_factor']
+    combined_data = comb_data[['country_name', 'commodity_name', 'month', 'year', 'price', 'country_status', 'price_factor', 'usd_price']]
+    return combined_data
 
-country_data = pd.read_excel("/Users/dp/Desktop/SEM 1/IS597PR/Final_Project/Datasets/Food_dataset/Country_status_price.xlsx")
-# country_data
 
-comb_data = food_data.merge(country_data, left_on = 'country_name', right_on = 'country_name')
-comb_data['usd_price'] = comb_data['price'] * comb_data['price_factor']
-# comb_data
+def select_data(combined_data, country1, country2, commodity):
 
-# Exclude unecessary columns
-combined_data = comb_data[['country_name', 'commodity_name', 'month', 'year', 'price', 'country_status', 'price_factor', 'usd_price']]
-# combined_data
-
-#Check the data type of all the columns
-# combined_data.dtypes
-
-def select_data(country1, country2, commodity):
     """
     Creates a dataframe based on the selected countries and commodity
+    :param combined_data: Dataframe containing all the columns
     :param country1: Name of developing country
     :param country2: Name of an underdeveloped country
     :param commodity: Name of a commodity
-    :return: a dataframe with the selected countries and commodity
-    >>> select_data("India", "Pakistan", "Wheat") # doctest : +NORMALIZE_WHITESPACE +ELLIPSIS
+    :return: a dataframe with the selected countries and
+    >>> temp_df = get_hyp1_data()
+    >>> select_data(temp_df, "India", "Pakistan", "Wheat") # doctest : +NORMALIZE_WHITESPACE +ELLIPSIS
             country_id country_name  ...  price_factor usd_price
     157245         115        India  ...        0.0130   0.16250
     157246         115        India  ...        0.0130   0.17550
@@ -49,7 +45,7 @@ def select_data(country1, country2, commodity):
     <BLANKLINE>
     [5929 rows x 21 columns]
    	"""
-    selective_data = combined_data.loc[((combined_data.country_name == country1) | (combined_data.country_name == country2)) & (food_data.commodity_name == commodity)]
+    selective_data = combined_data.loc[((combined_data.country_name == country1) | (combined_data.country_name == country2)) & (combined_data.commodity_name == commodity)]
     return selective_data
 
 def calc_diff(group: pd.DataFrame):
@@ -80,15 +76,17 @@ def calc_diff(group: pd.DataFrame):
     return group
 
 
-def plot_data(country1, country2, commodity):
+
+def plot_data(combined_data, country1, country2, commodity):
     """
     Plots a line graph with 'Year' on x-axis and the 'Price' of commodity on the y-axis
+    :param combined_data: Dataframe containing all the columns
     :param country1: Name of developing country
     :param country2: Name of an underdeveloped country
     :param commodity: Name of a commodity
     :return: Plots a line graph
     """
-    subset_data = select_data(country1, country2, commodity)
+    subset_data = select_data(combined_data, country1, country2, commodity)
     subset_data.sort_values(by='year', inplace=True)
 
     #     Find a common minimum and maximum year for the selected countries and commodity, to be set as x limits
@@ -117,9 +115,10 @@ def plot_data(country1, country2, commodity):
     plt.legend(loc=2, bbox_to_anchor=(1, 1), prop={"size": 20})
     plt.show()
 
-def calc_corr(country1, country2, commodity):
+def calc_corr(combined_data, country1, country2, commodity):
     """
     Calculates the correlation coefficient and p-value for the two countries and the selected commodity
+    :param combined_data: Dataframe containing all the columns
     :param country1: Name of developing country
     :param country2: Name of an underdeveloped country
     :param commodity: Name of a commodity
@@ -127,23 +126,19 @@ def calc_corr(country1, country2, commodity):
     >>> calc_corr("India", "Pakistan", "Wheat")
     (0.22274829066815113, 0.2640932178164745)
     """
-    _data = select_data(country1, country2, commodity)
-    q1, q2 = _data[_data['year'] == _data.groupby(_data['country_name'])['year'].min().max()], _data[
-        _data['year'] == _data.groupby(_data['country_name'])['year'].max().min()]
+    _data = select_data(combined_data, country1, country2, commodity)
+    q1, q2 = _data[_data['year']==_data.groupby(_data['country_name'])['year'].min().max()], _data[_data['year']==_data.groupby(_data['country_name'])['year'].max().min()]
     result = pd.concat(([q1, q2]))
-    a = len(result.loc[result['country_name'] == country1, 'usd_price'].tolist())
-    b = len(result.loc[result['country_name'] == country2, 'usd_price'].tolist())
+    a = len(result.loc[result['country_name']== country1, 'usd_price'].tolist())
+    b = len(result.loc[result['country_name']== country2, 'usd_price'].tolist())
     if a < b:
-        corr_value = pearsonr(result.loc[result['country_name'] == country1, 'usd_price'].tolist(),
-                              result.loc[result['country_name'] == country2, 'usd_price'].tolist()[:a])
+        corr_value = pearsonr(result.loc[result['country_name']== country1, 'usd_price'].tolist(), result.loc[result['country_name']== country2, 'usd_price'].tolist()[:a])
     elif a > b:
-        corr_value = pearsonr(result.loc[result['country_name'] == country1, 'usd_price'].tolist()[:b],
-                              result.loc[result['country_name'] == country2, 'usd_price'].tolist())
+        corr_value = pearsonr(result.loc[result['country_name']== country1, 'usd_price'].tolist()[:b], result.loc[result['country_name']== country2, 'usd_price'].tolist())
     else:
-        corr_value = pearsonr(result.loc[result['country_name'] == country1, 'usd_price'].tolist(),
-                              result.loc[result['country_name'] == country2, 'usd_price'].tolist())
+        corr_value = pearsonr(result.loc[result['country_name']== country1, 'usd_price'].tolist(), result.loc[result['country_name']== country2, 'usd_price'].tolist())
     return corr_value
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(verbose=True, optionflags=doctest.ELLIPSIS)
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod(verbose=True, optionflags=doctest.ELLIPSIS)
